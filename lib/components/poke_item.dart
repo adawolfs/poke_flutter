ce56a2bb855e7df937648ea6f074a26c3bb8bc66
aestrada <aestrada@digitalgeko.com>
2019-01-30 02:35:10 -0600

    added login option

--- lib/components/poke_item.dart
+++ lib/components/poke_item.dart
@@ -34,49 +34,54 @@ class _PokeItem extends State<PokeItem> {
     });
   }
 
+  Image loading() {
+    return Image.asset(
+      'assets/images/pokeball.png',
+      width: 100.0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (pokemon.length > 0) {
-      return new RaisedButton(
-        elevation: 0,
-        color: Theme.of(context).primaryColor,
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              decoration: new BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-              ),
-              child: Image.network(
-                pokemon['sprites']['front_default'],
-                Text(pokemon['name']),
-              ),
+    return new RaisedButton(
+      elevation: 0,
+      color: Theme.of(context).primaryColor,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
             ),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              padding: EdgeInsets.only(
-                bottom: 5.0,
-                top: 5.0,
-                left: 10.0,
-                right: 10.0,
-              ),
-              decoration: new BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(100.0)),
-              ),
-              child: Text(
-                capitalize(pokemon['name']),
-                style: Theme.of(context).textTheme.button,
-              ),
-            )
-          ],
-        ),
-        onPressed: () {},
-      );
-    } else {
-      return Text('loading');
-    }
+            child: pokemon.length > 0
+                ? Image.network(
+                    pokemon['sprites']['front_default'],
+                    Text(pokemon['name']),
+                  )
+                : loading(),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            padding: EdgeInsets.only(
+              bottom: 5.0,
+              top: 5.0,
+              left: 10.0,
+              right: 10.0,
+            ),
+            decoration: new BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.all(Radius.circular(100.0)),
+            ),
+            child: Text(
+              capitalize(pokemon.length > 0 ? pokemon['name'] : 'loading'),
+              style: Theme.of(context).textTheme.button,
+            ),
+          )
+        ],
+      ),
+      onPressed: () {},
+    );
   }
 }
